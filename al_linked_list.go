2dd7915177365f210d79a1e0330ed32a8394341f
Xiong <xiong11feng@sina.com>
2022-08-10 08:54:59 +0800

    单链表相关题目

--- al_linked_list.go
+++ al_linked_list.go
@@ -5,6 +5,7 @@ package main
 type SingalNode struct {
 	Next  *SingalNode
 	Value int
+	Rand  *SingalNode //另一个指针，指向一个随机的节点
 }
 
 //找到链表的中点
@@ -137,4 +138,206 @@ func ListPartition(head *SingalNode, a int) *SingalNode {
 		head = maxHead
 	}
 	return head
+}
+
+//复制带有random节点的链表
+//方法一：copy所有节点，通过map，存储对应关系（key，value）。根据key节点的指向，将value节点执行对应的克隆节点
+//方法二：将链表直接复制，a->b->c => a->a'->b->b'->c->c'; 再遍历，将随机节点指向，在x'上分配，最后返回x'
+func CopyListWithRandomNode(head *SingalNode) *SingalNode {
+	temp := head
+
+	if head == nil {
+		return nil
+	}
+
+	//1. a->b->c => a->a'->b->b'->c->c'
+	for temp != nil {
+		newNode := CopyNodeValue(temp)
+		next := temp.Next
+		temp.Next = newNode
+		newNode.Next = next
+		temp = next
+	}
+	//再copy随机节点
+	temp = head
+	for temp != nil {
+		next := temp.Next
+		if temp.Rand != nil {
+			next.Rand = temp.Rand.Next
+		} else {
+			next.Rand = nil
+		}
+		temp = next.Next
+	}
+
+	tailOld := head
+	res := head.Next
+	tail := res
+	temp = res.Next
+	for temp != nil {
+		tailOld.Next = temp
+		tailOld = temp
+		tail.Next = temp.Next
+		tail = temp.Next
+		temp = tail.Next
+	}
+	tailOld.Next = nil
+	return res
+}
+
+//仅仅拷贝值
+func CopyNodeValue(node *SingalNode) *SingalNode {
+	return &SingalNode{Value: node.Value}
+}
+
+func CompareNodeList(n1, n2 *SingalNode) bool {
+	temp1 := n1
+	temp2 := n2
+	for temp1 != nil && temp2 != nil {
+		if temp1.Value != temp2.Value {
+			return false
+		}
+		if temp1.Next == nil && temp2.Next != nil {
+			return false
+		}
+		if temp1.Next != nil && temp2.Next == nil {
+			return false
+		}
+		if temp1.Next != nil && temp2.Next != nil {
+			if temp1.Next.Value != temp2.Next.Value {
+				return false
+			}
+		}
+		if temp1.Rand == nil && temp2.Rand != nil {
+			return false
+		}
+		if temp1.Rand != nil && temp2.Rand == nil {
+			return false
+		}
+		if temp1.Rand != nil && temp2.Rand != nil {
+			if temp1.Rand.Value != temp2.Rand.Value {
+				return false
+			}
+		}
+		temp1 = temp1.Next
+		temp2 = temp2.Next
+	}
+	if temp1 != nil || temp2 != nil {
+		return false
+	}
+	return true
+}
+
+//【题目】给定两个可能有环，也可能无环的单链表，头节点head1，head2，请实现一个函数，如果两个链表相交，请返回相交的第一个节点，如果不想交，请返回null。
+//【要求】如果两个链表的长度之和是N，时间复杂度是O（N），额外空间复杂度达到O（1）
+func GetIntersectNode(node1, node2 *SingalNode) *SingalNode {
+	if node1 == nil || node2 == nil {
+		return nil
+	}
+	c1 := GetCircleListNode(node1)
+	c2 := GetCircleListNode(node2)
+
+	if c1 == nil && c2 == nil {
+		return noCircle(node1, node2)
+	} else if c1 != nil && c2 != nil {
+		return bothCircle(node1, node2, c1, c2)
+	}
+	return nil
+}
+
+//两个链表没有环
+func noCircle(node1, node2 *SingalNode) *SingalNode {
+	if node1 == nil || node2 == nil {
+		return nil
+	}
+	//相交的话，只有一种可能，相交后的所有节点都相同，如果相交，最后一个节点是相同的
+	temp1 := node1
+	temp2 := node2
+	len1 := 1
+	len2 := 1
+	for temp1.Next != nil {
+		temp1 = temp1.Next
+		len1++
+	}
+	for temp2.Next != nil {
+		temp2 = temp2.Next
+		len2++
+	}
+
+	//最后一个节点不同，表示不相交
+	if temp1 != temp2 {
+		return nil
+	}
+	temp1 = node1
+	temp2 = node2
+	if len1 > len2 {
+		for len1 != len2 {
+			temp1 = temp1.Next
+			len1--
+		}
+	} else if len1 < len2 {
+		for len1 != len2 {
+			temp2 = temp2.Next
+			len2--
+		}
+	}
+	for temp1 != temp2 {
+		temp1 = temp1.Next
+		temp2 = temp2.Next
+	}
+	return temp1
+}
+
+//两个链表都有环
+func bothCircle(node1, node2, circle1, circle2 *SingalNode) *SingalNode {
+	//链表的入环点相同, 可以将入环点作为末尾点，从而转换成无环的问题
+	if circle1 == circle2 {
+		temp1 := circle1.Next
+		temp2 := circle2.Next
+		circle1.Next = nil
+		circle2.Next = nil
+		res := noCircle(node1, node2)
+		circle1.Next = temp1
+		circle2.Next = temp2
+		return res
+	} else { //一定在环上有两个交点
+		temp := circle1.Next
+		for temp != circle1 {
+			if temp == circle2 {
+				return temp
+			}
+			temp = temp.Next
+		}
+		return nil
+	}
+}
+
+//判断链表有环无环，返回第一个入环节点，否则返回nil
+//使用快慢指针，空间复杂度最小
+//1.slow指针和fast指针，如果有环必相遇
+//2.相遇后快fast指针回到头，slow指针不动，同时以速度1前进，一定会在入环点相遇
+//大体的证明过程：https://blog.csdn.net/donghuaan/article/details/78988987
+//头到入环口距离k，此时快指针走了2k，其中后一个k走了n圈+delta ，所以 k = nR + delta
+//因为快指针距离slow指针式delta，所以在环上相遇，要多走 R-delta，所以快指针走2*（R-delta），所以相遇位置事2*（R-delta）+ delta = 2R-delta
+//k = nR + delta, k+delta = (2+n)R 所以在入口相遇
+func GetCircleListNode(node *SingalNode) *SingalNode {
+	head := node
+	if head == nil || head.Next == nil || head.Next.Next == nil {
+		return nil
+	}
+	fast := head.Next.Next
+	slow := head.Next
+	for slow != fast {
+		if fast.Next == nil || fast.Next.Next == nil {
+			return nil
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	fast = head
+	for slow != fast {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return slow
 }
--- al_linked_list_test.go
+++ al_linked_list_test.go
@@ -109,3 +109,82 @@ func TestListPartition(t *testing.T) {
 	assert.Equal(t, 5, actual.Next.Next.Next.Next.Value)
 	assert.Equal(t, 6, actual.Next.Next.Next.Next.Next.Value)
 }
+
+func TestCopyListWithRandomNode(t *testing.T) {
+	Node1 := &SingalNode{Value: 1}
+	Node2 := &SingalNode{Value: 2}
+	Node1.Next = Node2
+	Node3 := &SingalNode{Value: 3}
+	Node2.Next = Node3
+	Node4 := &SingalNode{Value: 4}
+	Node3.Next = Node4
+	Node5 := &SingalNode{Value: 5}
+	Node4.Next = Node5
+	Node6 := &SingalNode{Value: 6}
+	Node5.Next = Node6
+	Node1.Rand = Node5
+	Node2.Rand = Node6
+	Node3.Rand = Node1
+	Node4.Rand = Node2
+	Node5.Rand = nil
+	Node6.Rand = Node6
+	actual := CopyListWithRandomNode(Node1)
+	assert.Equal(t, true, CompareNodeList(Node1, actual))
+}
+
+func TestGetCircleListNode(t *testing.T) {
+	Node1 := &SingalNode{Value: 1}
+	Node2 := &SingalNode{Value: 2}
+	Node1.Next = Node2
+	Node3 := &SingalNode{Value: 3}
+	Node2.Next = Node3
+	Node4 := &SingalNode{Value: 4}
+	Node3.Next = Node4
+	Node5 := &SingalNode{Value: 5}
+	Node4.Next = Node5
+	Node6 := &SingalNode{Value: 6}
+	Node5.Next = Node6
+	Node6.Next = Node3
+	actual := GetCircleListNode(Node1)
+	assert.Equal(t, 3, actual.Value)
+}
+
+func TestGetIntersectNode(t *testing.T) {
+	Node1 := &SingalNode{Value: 1}
+	NodeIntersect := &SingalNode{Value: 2}
+	Node1.Next = NodeIntersect
+	Node1Circle := &SingalNode{Value: 3}
+	Node1.Next.Next = Node1Circle
+	Node1.Next.Next.Next = &SingalNode{Value: 4}
+	Node1.Next.Next.Next.Next = &SingalNode{Value: 5}
+	Node1.Next.Next.Next.Next.Next = Node1Circle
+
+	Node2 := &SingalNode{Value: 100}
+	Node2.Next = NodeIntersect
+
+	actual := GetIntersectNode(Node1, Node2)
+	assert.Equal(t, 2, actual.Value)
+
+	Node1.Next.Next.Next = nil
+	actual = GetIntersectNode(Node1, Node2)
+	assert.Equal(t, 2, actual.Value)
+
+	Node2.Next = &SingalNode{Value: 101}
+
+	actual = GetIntersectNode(Node1, Node2)
+	assert.Nil(t, actual)
+
+	Node2.Next = Node1Circle
+
+	actual = GetIntersectNode(Node1, Node2)
+	assert.Equal(t, 3, actual.Value)
+
+	Node2.Next = &SingalNode{Value: 101}
+	Node2Circle := &SingalNode{Value: 102}
+	Node2.Next.Next = Node2Circle
+	Node2.Next.Next.Next = &SingalNode{Value: 103}
+	Node2.Next.Next.Next.Next = Node2Circle
+
+	actual = GetIntersectNode(Node1, Node2)
+	assert.Nil(t, actual)
+}
